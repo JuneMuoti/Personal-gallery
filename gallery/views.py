fda417b8e19ac3e3d87f1a45a20c57e019d3c602
June Muoti <june.muoti@gmail.com>
2018-02-26 12:27:35 +0300

    edit search function

--- gallery/views.py
+++ gallery/views.py
@@ -18,10 +18,9 @@ def contact(request):
 
 
 def search_results(request):
-
     if 'image' in request.GET and request.GET["image"]:
         search_term = request.GET.get("image")
-        searched_images = Image.search_by_category(search_term)
+        searched_images = Image.search_by_category(query)
         message = f"{search_term}"
 
         return render(request, 'search.html',{"message":message,"image": searched_images})
