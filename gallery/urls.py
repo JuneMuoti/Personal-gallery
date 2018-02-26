52ab697a054241057b0606c65a411b67e1d82e4b
June Muoti <june.muoti@gmail.com>
2018-02-26 08:24:01 +0300

    add url pattern for search results and imageid

--- gallery/urls.py
+++ gallery/urls.py
@@ -7,7 +7,9 @@ urlpatterns=[
     url(r'^$',views.index,name = 'index'),
     url(r'^about/$',views.about,name = 'about'),
     url(r'^contact/$',views.contact,name = 'contact'),
-    url(r'^search/', views.search_results, name='search_results')
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^image/(\d+)',views.get_image,name ='image'),
+
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
